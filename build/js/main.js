b7e165d619cd3992fdebd8646d49e438524c0de6
Oscar Elizondo <oscarmarcelo95@gmail.com>
2015-11-03 23:04:23 -0600

    adds parallaxing:

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,14 @@
-(function() {
-  $(function() {});
-
-}).call(this);
+(function(){
+  $(window).scroll(function(){
+    var scroll = $(this).scrollTop();
+    $('.title').css({
+      'transform': 'translate(0px, ' + scroll/2 + '%)'
+    });
+    $('.ribbon').css({
+      'transform': 'translate(0px, ' + scroll/10 + '%)'
+    });
+    $('.subtitle').css({
+      'transform': 'translate(0px, ' + scroll/3 + '%)'
+    });
+  });
+})();
